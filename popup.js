b7451bfdcadf414958914f1fbbbaa00cc76de5fb
pierre baele <pierre@MacBook-Pro-3.local>
2023-12-03 13:54:47 +0100

    final achievement with papa

--- popup.js
+++ popup.js
@@ -9,22 +9,24 @@ function calculateCorrelation() {
   var text1 = document.getElementById("text1").value;
   var text2 = document.getElementById("text2").value;
 
-  // Function to calculate correlation percentage
-  var correlationPercentage = calculatePercentage(text1, text2);
+  // Fonction pour diviser le texte en mots et les convertir en minuscules
+  function getWords(text) {
+    return text.toLowerCase().match(/\b\w+\b/g) || [];
+  }
 
-  // Display the result
+  // Obtenir la liste des mots pour chaque texte
+  var words1 = getWords(text1);
+  var words2 = getWords(text2);
+
+  // Obtenir le nombre de mots identiques
+  var commonWords = words1.filter((word) => words2.includes(word));
+
+  // Calculer le pourcentage de corrélation
+  var correlationPercentage =
+    (commonWords.length / Math.max(words1.length, words2.length)) * 100;
+
+  // Afficher le résultat
   var resultDiv = document.getElementById("result");
   resultDiv.innerHTML =
-    "Correlation Percentage: " + correlationPercentage + "%";
-}
-
-function calculatePercentage(text1, text2) {
-  // Implement your correlation calculation logic here
-  // This can be a simple algorithm or a more sophisticated one based on your needs
-  // Return the calculated percentage
-  // For simplicity, let's assume a basic algorithm that compares the lengths of the two texts
-  var maxLength = Math.max(text1.length, text2.length);
-  var minLength = Math.min(text1.length, text2.length);
-  var percentage = (minLength / maxLength) * 100;
-  return percentage.toFixed(2); // Round to two decimal places
+    "Correlation Percentage: " + correlationPercentage.toFixed(2) + "%";
 }
