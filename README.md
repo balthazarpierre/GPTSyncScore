6604505271cc82cd006c65ddd27f773e459c8d66
balthazarpierre <balthazar.guide@gmail.com>
2023-12-07 07:57:38 +0100

    extension improvements and fix bug in calculation

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# ChatGPT chrome extension
+# GPTSyncScore chrome extension
 
-goal : measure pourcentage of correlation between initial text and final text generated wiht chatgpt
+🙏 Feel at ease to submit PR
+
+Idea behind the extension : [Linkedin Post](https://www.linkedin.com/posts/pierre-baele_chatgpt-anniv-keepitreal-activity-7136227603769634817-rELS?utm_source=share&utm_medium=member_desktop)
+
+I have been playing around with ChatGPT for 3 weeks now, and I went from pessimistic to optimistic about the impact it can have on society. But for that, I think we need transparency about ChatgptUsage
+
+🟢 MVP : In clicking the extension, a popup appears where I can enter the two texts. I click on the 'SyncScore' button, and the percentage is copied to the clipboard.
+
+🎯 Target feature : I am in the ChatGPT interface, entering my text into ChatGPT (with a prompt ID?). I iterate on the modification, and once satisfied with the result, I click on the extension, and the SyncScore is directly copied to my clipboard. Additionally, there is an option available where one can visually see the difference between the two texts.
+
+ℹ️ Code is not so good right now because it was mostly generated with chatGPT 😅
+
+## TODOS
+
+- [] Improve GPTSyncScore calculation
+- [] Add (CI) tests
+- [] Better ui/ux
+
+## QA
+
+- no words in common => GPTSyncScore 100%
+- all words are the same => GPTSyncScore 0%
+- orginal text "toto est" / final text "toto est" => GPTSyncScore 50%
--- popup.js
+++ popup.js
@@ -1,32 +1,70 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Sélectionnez les éléments input et le bouton dans le DOM
+  var inputText1 = document.getElementById("originalText");
+  var inputText2 = document.getElementById("finalText");
   var calculateButton = document.getElementById("calculateButton");
+
+  // Ajoutez des gestionnaires d'événements pour les changements dans les champs de saisie
+  inputText1.addEventListener("input", recalculateCorrelation);
+  inputText2.addEventListener("input", recalculateCorrelation);
+
+  // Ajoutez un gestionnaire d'événements pour le bouton de calcul
   calculateButton.addEventListener("click", function () {
-    calculateCorrelation();
+    recalculateCorrelation();
+    copyResultToClipboard();
   });
 });
 
-function calculateCorrelation() {
-  var text1 = document.getElementById("text1").value;
-  var text2 = document.getElementById("text2").value;
+function recalculateCorrelation() {
+  // Obtenez les valeurs actuelles des champs de saisie
+  var text1 = document.getElementById("originalText").value;
+  var text2 = document.getElementById("finalText").value;
 
-  // Fonction pour diviser le texte en mots et les convertir en minuscules
-  function getWords(text) {
-    return text.toLowerCase().match(/\b\w+\b/g) || [];
-  }
+  // Appelez la fonction pour recalculer le pourcentage de corrélation
+  var correlationPercentage = calculatePercentage(text1, text2);
 
-  // Obtenir la liste des mots pour chaque texte
+  // Mettez à jour l'affichage du résultat
+  document.getElementById("result").innerHTML =
+    "#GPTSyncScore: " + correlationPercentage + "%";
+}
+
+function calculatePercentage(text1, text2) {
   var words1 = getWords(text1);
   var words2 = getWords(text2);
 
-  // Obtenir le nombre de mots identiques
   var commonWords = words1.filter((word) => words2.includes(word));
+  var percentage;
 
-  // Calculer le pourcentage de corrélation
-  var correlationPercentage =
-    (commonWords.length / Math.max(words1.length, words2.length)) * 100;
+  if (words1.length === 0 && words2.length === 0) {
+    // Les deux champs sont vides, donc 0%
+    percentage = 0;
+  } else {
+    // Calculer le pourcentage en tenant compte du nombre de mots communs
+    var totalUniqueWords = new Set([...words1, ...words2]).size;
+    percentage = (commonWords.length / totalUniqueWords) * 100;
+  }
 
-  // Afficher le résultat
-  var resultDiv = document.getElementById("result");
-  resultDiv.innerHTML =
-    "Correlation Percentage: " + correlationPercentage.toFixed(2) + "%";
+  return percentage.toFixed(2);
+}
+
+function getWords(text) {
+  // Diviser le texte en mots sans tenir compte des espaces
+  return text
+    .toLowerCase()
+    .split(/\s+/)
+    .filter((word) => word.length > 0);
+}
+
+function copyResultToClipboard() {
+  // Sélectionnez le résultat
+  var resultElement = document.getElementById("result");
+  var resultText = resultElement.textContent || resultElement.innerText;
+
+  // Créez un élément temporaire de texte de formulaire pour copier le texte dans le presse-papiers
+  var tempInput = document.createElement("textarea");
+  tempInput.value = resultText;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
 }
